90b12360e5863ddc42714a7af38e504281e233a3
PGBSUMANB <74059179+PGBSUMANB@users.noreply.github.com>
2020-12-17 08:25:42 +0530

    Add files via upload

---
+++ process/scripts/WFComplaint/determinemail.js
@@ -0,0 +1,36 @@
+/*
+// read from existing workflow context 
+var productInfo = $.context.productInfo; 
+var productName = productInfo.productName; 
+var productDescription = productInfo.productDescription;
+
+// read contextual information
+var taskDefinitionId = $.info.taskDefinitionId;
+
+// read user task information
+var lastUserTask1 = $.usertasks.usertask1.last;
+var userTaskSubject = lastUserTask1.subject;
+var userTaskProcessor = lastUserTask1.processor;
+var userTaskCompletedAt = lastUserTask1.completedAt;
+
+var userTaskStatusMessage = " User task '" + userTaskSubject + "' has been completed by " + userTaskProcessor + " at " + userTaskCompletedAt;
+
+// create new node 'product'
+var product = {
+		productDetails: productName  + " " + productDescription,
+		workflowStep: taskDefinitionId
+};
+
+// write 'product' node to workflow context
+$.context.product = product;
+*/
+var i = $.context.ApprovalLevel - 1 + 1;
+
+var currentUserName1 = $.context.ApproverIdRes.Result[0].ApproverIdRes[i].ApproverId;
+
+var UserEmailIf1 = {
+		"UserName": currentUserName1
+	};
+
+
+$.context.UserEmailIdIf1 = UserEmailIf1;
