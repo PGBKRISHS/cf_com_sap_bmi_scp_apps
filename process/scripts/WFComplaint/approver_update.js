5ca0ed13ec0dfdb23ece69d53b8cf34d91197700
PGBSUMANB <74059179+PGBSUMANB@users.noreply.github.com>
2020-12-17 08:18:40 +0530

    Update approver_update.js

--- process/scripts/WFComplaint/approver_update.js
+++ process/scripts/WFComplaint/approver_update.js
@@ -41,25 +41,6 @@ $.context.product = product;
 }
 			};
 $.context.StatusUpdate = status;*/
-if ($.context.ApprovalLevel1 == "0"){
-	$.context.ApprovalLevel1 = "1";
-}
-else if ($.context.ApprovalLevel1 == "1"){
-$.context.StatusUpdate.SettlementTrackingUpdate.ApprovalList.Level1 = $.context.ApproverId.Result[0].ApproverId.ApproverId + "(To be Approved)";
-	$.context.ApprovalLevel1 = "2";
-}
-else if ($.context.ApprovalLevel1 == "2"){
-$.context.StatusUpdate.SettlementTrackingUpdate.ApprovalList.Level2 = $.context.ApproverId.Result[0].ApproverId.ApproverId + "(To be Approved)";
-	$.context.ApprovalLevel1 = "3";
-}
-else if ($.context.ApprovalLevel1 == "3"){
-$.context.StatusUpdate.SettlementTrackingUpdate.ApprovalList.Level3 = $.context.ApproverId.Result[0].ApproverId.ApproverId + "(To be Approved)";
-	$.context.ApprovalLevel1 = "4";
-}
-else if ($.context.ApprovalLevel1 == "4"){
-$.context.StatusUpdate.SettlementTrackingUpdate.ApprovalList.Level4 = $.context.ApproverId.Result[0].ApproverId.ApproverId + "(To be Approved)";
-	$.context.ApprovalLevel1 = "5";
-}
 
 var ApproverIf = {"RuleServiceId": "8c8352a976a34857a8ea7b68be9a4eb9",
 	"Vocabulary": [{
@@ -67,8 +48,7 @@ var ApproverIf = {"RuleServiceId": "8c8352a976a34857a8ea7b68be9a4eb9",
 	"Country": $.context.Messages.Message2.ServiceRequestPartyCollection.ServiceRequestParty.CountryCode,
 	"ComplaintCategory": $.context.Messages.Message1.ServiceRequestCollection.ServiceRequest.ServiceCategoryID,
 	"ComplaintStatus": $.context.Messages.Message1.ServiceRequestCollection.ServiceRequest.ServiceRequestUserLifeCycleStatusCode,
-	"ImmediateSettlement": $.context.Messages.Message1.ServiceRequestCollection.ServiceRequest.Z_ImmediateSettlement_Header_KUT,
-	"ApprovalLevel": $.context.ApprovalLevel1
+	"ImmediateSettlement": $.context.Messages.Message1.ServiceRequestCollection.ServiceRequest.Z_ImmediateSettlement_Header_KUT
 		}
 	}]};
 $.context.ApproverIdIf = ApproverIf;
